029965a01774dbb8fe0a46446bb66c8344952306
Edel <edelalexanderfb@gmail.com>
2017-10-19 04:18:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/admin/firebasestorage/ImageFragment.java
@@ -0,0 +1,64 @@
+package com.example.admin.firebasestorage;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+
+import static android.app.Activity.RESULT_OK;
+import static android.content.ContentValues.TAG;
+
+
+public class ImageFragment extends Fragment {
+
+    private static final int GALLERY_INTENT = 2;
+    private ImageView iView;
+    private Button btnOpenGal;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_image, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        iView = view.findViewById(R.id.imageView);
+        btnOpenGal = view.findViewById(R.id.btnOpenGal);
+
+        btnOpenGal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_INTENT);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            final Uri uri = data.getData();
+
+            iView.setImageURI(uri);
+        }
+    }
+}
